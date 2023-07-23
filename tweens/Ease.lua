8f0791dbdcb43e85bedc4819583c58ef5269a67b
swordcube <49110074+swordcube@users.noreply.github.com>
2023-07-22 21:42:46 -0500

    fix some of the eases

--- tweens/Ease.lua
+++ tweens/Ease.lua
@@ -20,14 +20,17 @@ Ease = {
     linear = function(v) return v end,
     
     backIn = function(v) return v^2*(2.70158*v-1.70158) end,
-    backOut = function(v) return 1-(v-1)*v*(-2.70158*v-1.70158) end,
+    backOut = function(v) 
+        v = v - 1
+        return 1-v*v*(-2.70158*v-1.70158) 
+    end,
     backInOut = function(v)
-        v = v*2;
+        v = v*2
 		if (v < 1) then
-			return v^2*(2.70158*v-1.70158)/2;
+			return v^2*(2.70158*v-1.70158)/2
         end
 		v = v-1
-		return (1-(v-1)*(v)*(-2.70158*v-1.70158))/2+0.5;
+		return (1-(v-1)*(v)*(-2.70158*v-1.70158))/2+0.5
     end,
 
     bounceIn = function(v)
@@ -36,12 +39,12 @@ Ease = {
 			return 1 - 7.5625 * v * v
         end
 		if (v < Ease.B2) then
-			return 1 - (7.5625 * (v - Ease.B3) * (v - Ease.B3) + 0.75);
+			return 1 - (7.5625 * (v - Ease.B3) * (v - Ease.B3) + 0.75)
         end
 		if (v < Ease.B4) then
-			return 1 - (7.5625 * (v - Ease.B5) * (v - Ease.B5) + 0.9375);
+			return 1 - (7.5625 * (v - Ease.B5) * (v - Ease.B5) + 0.9375)
         end
-		return 1 - (7.5625 * (v - Ease.B6) * (v - Ease.B6) + 0.984375);
+		return 1 - (7.5625 * (v - Ease.B6) * (v - Ease.B6) + 0.984375)
     end,
     bounceOut = function(v) return 1-Ease.bounceIn(1-v) end,
     bounceInOut = function(v)
@@ -57,16 +60,16 @@ Ease = {
 
     elasticIn = function(v)
         v = v-1
-        return -(Ease.E_A * math.pow(2, 10 * v) * math.sin((v - (Ease.E_P / (2 * math.pi) * math.asin(1 / Ease.E_A))) * (2 * math.pi) / Ease.E_P));
+        return -(Ease.E_A * math.pow(2, 10 * v) * math.sin((v - (Ease.E_P / (2 * math.pi) * math.asin(1 / Ease.E_A))) * (2 * math.pi) / Ease.E_P))
     end,
-    elasticOut = function(v) return (Ease.E_A * math.pow(2, -10 * v) * math.sin((v - (Ease.E_P / (2 * math.pi) * math.asin(1 / Ease.E_A))) * (2 * math.pi) / Ease.E_P) + 1); end,
+    elasticOut = function(v) return (Ease.E_A * math.pow(2, -10 * v) * math.sin((v - (Ease.E_P / (2 * math.pi) * math.asin(1 / Ease.E_A))) * (2 * math.pi) / Ease.E_P) + 1) end,
     elasticInOut = function(v)
         if (v < 0.5) then
             v = v-0.5
-			return -0.5 * (math.pow(2, 10 * v) * math.sin((v - (Ease.E_P / 4)) * (2 * math.pi) / Ease.E_P));
+			return -0.5 * (math.pow(2, 10 * v) * math.sin((v - (Ease.E_P / 4)) * (2 * math.pi) / Ease.E_P))
 		end
         v = v-0.5
-		return math.pow(2, -10 * v) * math.sin((v - (Ease.E_P / 4)) * (2 * math.pi) / Ease.E_P) * 0.5 + 1;
+		return math.pow(2, -10 * v) * math.sin((v - (Ease.E_P / 4)) * (2 * math.pi) / Ease.E_P) * 0.5 + 1
     end,
 
     expoIn = function(v) return 2^(10*(v-1)) end,
@@ -98,7 +101,10 @@ Ease = {
     quintInOut = function(v) return v < 0.5 and 16*v^5 or 1-(-2 * v + 2)^5/2 end,
 
     cubeIn = function(v) return v^3 end,
-    cubeOut = function(v) return 1+(v-1)*(v^2) end,
+    cubeOut = function(v) 
+        v = v - 1
+        return 1+v*(v^2)
+    end,
     cubeInOut = function(v) return v <= 0.5 and (v^3)*4 or 1+(v-1)*(v^2)*4 end,
 }
 return Ease
